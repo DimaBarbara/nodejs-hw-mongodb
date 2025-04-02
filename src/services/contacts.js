93d5854a6209efc6821887fb401b62101259f905
Dima <oxxTerm0s@gmail.com>
2025-04-02 23:27:01 +0300

    hw2mongo

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -3,3 +3,24 @@ import ContactCollection from '../db/models/Contact.js';
 export const getContacts = () => ContactCollection.find();
 
 export const getContactsById = (id) => ContactCollection.findOne({ _id: id });
+
+export const postContact = (payload) => ContactCollection.create(payload);
+
+export const patchContact = async (id, payload, options = {}) => {
+  const result = await ContactCollection.findByIdAndUpdate(
+    { _id: id },
+    payload,
+    {
+      new: true,
+      includeResultMetadata: true,
+      ...options,
+    },
+  );
+  if (!result || result.value) return null;
+  return {
+    contact: result.value,
+    isNew: Boolean(result?.lastErrorObject?.upserted),
+  };
+};
+
+export const deleteContact = (id) => ContactCollection.findOneAndDelete({ _id: id });
